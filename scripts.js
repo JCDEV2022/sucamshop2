d2e22f080ef49e58e11f3039fa18c2bae4cd73f8
juan camilo medina <milomed2012@gmail.com>
2025-10-13 21:42:55 -0500

    menu hambu

---
+++ scripts.js
@@ -0,0 +1,10 @@
+<script>
+  // Mostrar/Ocultar menú en móvil
+  const menuIcon = document.getElementById("menu-icon");
+  const nav = document.getElementById("nav");
+
+  menuIcon.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    menuIcon.classList.toggle("open");
+  });
+</script>
